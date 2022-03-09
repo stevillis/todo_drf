cd5aa88186970cec6b297391c61aa9ef59a4a823
Stévillis Sousa <stevillis@hotmail.com>
2022-03-09 01:41:30 -0400

    Create api_overview view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,14 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def api_overview(request):
-    return JsonResponse('API Base Point', safe=False)
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>',
+        'Delete': '/task-delete/<str:pk>',
+    }
+    return Response(api_urls)
