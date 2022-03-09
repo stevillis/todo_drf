50a3bbc0620834f9cc55ab7855349426c2da2e47
Stévillis Sousa <stevillis@hotmail.com>
2022-03-09 01:53:39 -0400

    List all Tasks

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='api-overview'),
+    path('task-list/', views.task_list, name='task-list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Task
+from .serializers import TaskSerializer
+
 
 @api_view(['GET'])
 def api_overview(request):
@@ -12,3 +15,10 @@ def api_overview(request):
         'Delete': '/task-delete/<str:pk>',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
