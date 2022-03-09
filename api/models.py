88f8fdd69605f5f41123774b71debd5c95cbbe25
Stévillis Sousa <stevillis@hotmail.com>
2022-03-09 01:33:36 -0400

    Create Task Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Task(BaseModel):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
