7beb47709c3d9a71564c2c6398dca5034712ac95
Stévillis Monteiro de Sousa <stevillis.sousa@neuralmind.ai>
2022-07-18 18:59:23 -0400

    fix: update django heroku for linux version

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -13,7 +13,7 @@ import os
 from decouple import config
 from pathlib import Path
 
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +139,4 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
